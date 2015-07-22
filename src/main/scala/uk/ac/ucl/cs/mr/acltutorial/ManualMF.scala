a05fa60762be09fa7b18a83f559a960d81d9f24e
riedelcastro <sebastian.riedel@gmail.com>
2015-07-22 20:40:09 +0100

    SVD

--- src/main/scala/uk/ac/ucl/cs/mr/acltutorial/ManualMF.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/acltutorial/ManualMF.scala
@@ -1,5 +1,6 @@
 package uk.ac.ucl.cs.mr.acltutorial
 
+import breeze.linalg.{svd, DenseMatrix}
 import cc.factorie.la.{DenseTensor2, DenseTensor1}
 import ml.wolfe.{Mat, Vect}
 import ml.wolfe.term.{IntTerm, domain}
@@ -81,8 +82,16 @@ object ManualMF {
     Matrix(for (i <- (0 until M.dim1).toList; j <- (0 until M.dim2).toList) yield Cell(i, j, M(i, j)))
   }
 
+  def colVector(v: Vect) = {
+    Matrix(for (i <- 0 until v.dim1) yield Cell(i, 0, v(i)))
+  }
+
+  def rowVector(v: Vect) = {
+    Matrix(for (i <- 0 until v.dim1) yield Cell(0, i, v(i)))
+  }
+
   def numbers(m: Matrix) = {
-    m.copy(m.cells collect { case cell@Cell(_, _, d: Double, _, _, _,_) => cell.copy(value = f"$d%2.2f") })
+    m.copy(m.cells collect { case cell@Cell(_, _, d: Double, _, _, _, _) => cell.copy(value = f"$d%2.2f") })
   }
 
   def opacity(m: Matrix, min: Double = -1, max: Double = 1, color: (Int, Int, Int) = (0, 0, 0)) = {
@@ -90,7 +99,7 @@ object ManualMF {
       val capped = Math.min(Math.max(min, value), max)
       (capped - min) / (max - min)
     }
-    m.copy(m.cells collect { case cell@Cell(_, _, d: Double, _, _, _,_) =>
+    m.copy(m.cells collect { case cell@Cell(_, _, d: Double, _, _, _, _) =>
       cell.copy(value = "", color = color, opacity = valueToOpacity(d))
     })
   }
@@ -108,14 +117,14 @@ object ManualMF {
     val rows = string.split("\n").map(_.trim.split("\\s"))
     val n = rows.length
     val m = rows.head.length
-    val cells = for (i <- 0 until n; j <- 0 until m; if rows(i)(j) != "_" ) yield Cell(i,j,rows(i)(j))
+    val cells = for (i <- 0 until n; j <- 0 until m; if rows(i)(j) != "_") yield Cell(i, j, rows(i)(j))
     Matrix(cells)
   }
 
-  def sigm(mat:Mat) = {
-    val result = new DenseTensor2(mat.dim1,mat.dim2)
+  def sigm(mat: Mat) = {
+    val result = new DenseTensor2(mat.dim1, mat.dim2)
     for (i <- 0 until mat.dim1; j <- 0 until mat.dim2)
-      result(i,j) = ml.wolfe.util.Math.sigmoid(mat(i,j))
+      result(i, j) = ml.wolfe.util.Math.sigmoid(mat(i, j))
     result
   }
 
@@ -130,6 +139,31 @@ object ManualMF {
     val plusColEmbeddings = V.indices.map(j => colEmbedding(j, rows, V(j).toSeq, withBoxes)).foldLeft(plusRowEmbeddings)(_ + _)
     plusColEmbeddings.copy(hRulers = Seq(rows), vRulers = Seq(cols))
   }
+
+  def breezeSVD(mat: Mat):(Mat,Vect,Mat) = {
+    val asBreeze = toBreezeMat(mat)
+    val (u, s, v) = svd(asBreeze)
+    (toFactorieMat(u), new DenseTensor1(s.toArray), toFactorieMat(v))
+  }
+
+  def toEmbeddings(u:Mat, s:Vect, v:Mat, k:Int):(Seq[Vect],Seq[Vect]) = {
+    val _a = for (i <- 0 until u.dim1) yield new DenseTensor1(k)
+    val _v = for (i <- 0 until v.dim1) yield new DenseTensor1(k)
+    for (i <- _a.indices; l <- 0 until k) _a(i)(l) = u(i,l) * math.sqrt(s(l))
+    for (j <- _v.indices; l <- 0 until k) _v(j)(l) = v(l,j) * math.sqrt(s(l))
+    (_a,_v)
+  }
+
+  def toBreezeMat(mat: Mat) = {
+    DenseMatrix.create(mat.dim1, mat.dim2, mat.toArray).t
+  }
+
+  def toFactorieMat(mat: DenseMatrix[Double]) = {
+    val result = new DenseTensor2(mat.rows, mat.cols)
+    for (i <- 0 until mat.rows; j <- 0 until mat.cols) result(i, j) = mat(i, j)
+    result
+  }
+
 
   def main(args: Array[String]) {
     //    val n = 5
@@ -149,6 +183,21 @@ object ManualMF {
        0 0 0 1 1
        0 0 1 1 1
        1 2 1 1 1""")
+    val m = toBreezeMat(M)
+
+    val (u, s, v) = breezeSVD(M)
+    println(s)
+
+    val (_a,_v) = toEmbeddings(u,s,v,4)
+    println(dots(_a,_v))
+
+    //
+    //    println(m)
+    //    println(toFactorieMat(m))
+    //
+    //    val bM = DenseMatrix((1.0,2.0), (2.0,4.0))
+    //    val (u,s,v) = svd(bM)
+    //    println(s)
 
   }
 
